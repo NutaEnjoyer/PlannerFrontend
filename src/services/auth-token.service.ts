02ab39c5df3cc748b632aed476199f070ce73f8c
Max <mdolgov322@gmail.com>
2025-05-27 20:31:23 +0400

    replace localhost

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
     Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, { 
-        domain: 'localhost',
+        domain: '95.163.229.128',
         sameSite: 'strict',
         expires: 1,
     });
